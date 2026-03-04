3a79c7ede411335f1485fab6c7f3b6e07e31d9c8
sthefanyvillafranca <svillsei@myuax.com>
2026-03-04 10:18:51 +0100

    feat(EstacioEspacial): Atributos y metodo constructor

--- src/EstacionEspacial.java
+++ src/EstacionEspacial.java
@@ -1,4 +1,18 @@
 package PACKAGE_NAME;
 
 public class EstacionEspacial {
-}
+
+    private Nave[] navesAtracadas;
+    private final int NUM_MAX_NAVES;
+
+
+    /**
+     * Metodo constructor
+     * @return EstacionEspacial
+     */
+    public EstacionEspacial() {
+
+        NUM_MAX_NAVES = 20;
+        navesAtracadas = new Nave[NUM_MAX_NAVES];
+
+    }
